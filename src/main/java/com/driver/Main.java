a53c65a1486972eab6f6205dd4ca958c086e47ab
Param-Veer-Singh <paramveersinghchampawat@gmail.com>
2023-07-11 12:25:26 +0530

    encapsulation task done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+       // obj.name = 1;
+        //System.out.println(obj.name);
+        //java: name has private access in com.driver.RWOnly
+
+        obj.setter(1);
+        System.out.println(obj.getter());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int name;
+
+    public int getter(){
+        return name;
+    }
+    public void setter(int x){
+        name = x;
+    }
+
 }
